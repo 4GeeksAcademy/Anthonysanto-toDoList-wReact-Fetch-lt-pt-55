b4b8c04c362d9b8bdd33044b480d051cf59f3622
Anthony González <asgtoribio@gmail.com>
2025-07-22 01:38:57 +0000

    finalizado

--- src/js/components/TodoList.jsx
+++ src/js/components/TodoList.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import '/src/styles/index.css';
 
 const ToDoList = () => {
   const [task, setTask] = useState([]);
@@ -18,27 +19,34 @@ const ToDoList = () => {
     }
   }
 
+  const deleteTask = (index) => {
+  setTask(task.filter((_, i) => i !== index));  
+  };
+
   return (
     <>
-      <h1>Lista de tareas</h1>
+    <h1 className='d-flex flex-column justify-content-center align-items-center fw-light' style={{color:'gray'}}>Todos</h1>
 
 
-     <div className='d-flex flex-column justify-content-center align-items-center bg-secondary'>
-        <div className="card" style={{width: '18rem'}}>
-                <div className="card-header">
-                    <ul className="list-group list-group-flush">
+      <div className='d-flex flex-column justify-content-center align-items-center'>
+          <div className="card" style={{width: '40rem'}}>
+                  <div className="card-header">
+                      <ul className="list-group list-group-flush">
 
-                    <input type="text" value={inputValue} onChange={(e) => setInputValue(e.target.value)}  onKeyDown={handleKeyDown} 
-                    placeholder="Escribe una tarea" style={{border:'none', outline:'none'}}/>
-                        {task.map((elemento, index) => (
-                          <li key={index} className="list-group-item" >{elemento}
-                          </li>
-                    ))}
-                    </ul>
-                    <p className="d-flex mb-0 text-body-secondary" style={{fontSize:'10px'}}>Tareas pendientes {task.length}</p>
-                </div>
+                        <input type="text" value={inputValue} onChange={(e) => setInputValue(e.target.value)}  onKeyDown={handleKeyDown} 
+                        placeholder="Escribe una tarea" style={{border:'none', outline:'none'}}/>
+                            {task.map((elemento, index) => (
+                              <li key={index} className="list-group-item text-secondary d-flex justify-content-between align-items-center">{elemento}
+                                <span className='delete' onClick={()=>deleteTask(index)}  >X</span>
+                              </li>
+                        ))}
+                      </ul>
+                      <footer className="footer">tareas pendientes {task.length}</footer>
+                  </div>
+                  <div className="fondo-inferior"></div>
+                  <div className="fondo-inferior2"></div>
+          </div>
         </div>
-      </div>
     </>
   );
 };
